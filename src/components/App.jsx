d3198c241c5e4c14f3dfb5c43668e6107d2ec22e
Dina Karan <dina@VijayaKumar>
2022-10-21 14:57:03 +0530

    first

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profiles } from './index';
+import useStyles from './styles';
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profiles/:id" element={<Profiles />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
